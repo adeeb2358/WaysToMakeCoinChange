f1dd94ec47cda16553dccd4311b9af5e99c5695a
Adeeb Mohammed <adeeb2358@gmail.com>
2020-02-13 01:11:19 +0900

    Initial Commit

---
+++ WaysToMakeCoinChange/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WaysToMakeCoinChange
+//
+//  Created by adeeb mohammed on 13/02/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
